f2e731eaf9cb2de4ac6d6c5917d7d6b2389295a2
Anish Sridhar <sridhar.an@husky.neu.edu>
2020-08-19 06:16:19 -0400

    Completed CC-11

---
+++ remove_K_digits.java
@@ -0,0 +1,36 @@
+
+//TC - O(N)
+//SC - O(N)
+import java.util.*;
+
+class Solution1 {
+	public String removeKdigits(String num, int k) {
+		int size = num.length();
+		if (k == size)
+			return "0";
+		Stack<Character> stack = new Stack<>();
+		int count = 0;
+		while (count < size) {
+			while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(count)) {
+				stack.pop();
+				k--;
+			}
+			stack.push(num.charAt(count));
+			count++;
+		}
+		while (k > 0) {
+			stack.pop();
+			k--;
+		}
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty()) {
+			char curr = stack.pop();
+			sb.append(curr);
+		}
+		sb.reverse();
+		while (sb.length() > 1 && sb.charAt(0) == '0') {
+			sb.deleteCharAt(0);
+		}
+		return sb.toString();
+	}
+}
