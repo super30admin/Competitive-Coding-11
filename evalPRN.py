d4fefdab2dfe49032a178a219296bb40b6dcd10c
Yash Kulkarni <yvk@users.noreply.github.com>
2021-10-03 11:22:46 -0700

    CC11- Reverse Polish notation

---
+++ evalPRN.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    #Time complexity O(n)
+    #space complexity O(n)
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        result_stack= []
+        for element in tokens:
+            if element not in "+-*/":
+                result_stack.append(int(element))
+                continue
+            recent1 = result_stack.pop()
+            recent2 = result_stack.pop()
+            result = 0
+            if element == "+":
+                result = recent1 +recent2
+            elif element =="-":
+                result = recent2 - recent1
+            elif element == "*":
+                result = recent1*recent2
+            elif element == "/":
+                result = int(float(recent2) /recent1)
+            
+            result_stack.append(result)
+        return result_stack.pop()
+                
+        
